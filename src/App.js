c2cfaf13a37b540a3c9ceb86a69c077e16c1ecbe
Manuel Casado <ManuelCasado@protonmail.com>
2022-03-29 13:47:47 +0200

    Agregado primer state. No funciona bien

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 
+import { useState } from 'react';
 import './App.css';
+import Letras from "./components/Letras";
+import Palabras from "./components/Palabras";
+
+
 
 function App() {
+
+  const [ textAreaContent, setTextAreaContent ] = useState("");
+
+
+  function changeTextHandler (event) {
+    setTextAreaContent (event.target.value);
+    console.log(textAreaContent);
+  }
+
+  
   return (
     <>
       <h1>Cuenta Letras</h1>
+      
       <textarea 
+          onChange={changeTextHandler}
           placeholder='Introduzca el texto a contar...' 
           maxLength={500}
           rows={8} 
           cols={80}>
       </textarea>
+      
       <Letras Letras={Letras}/>
       <Palabras Palabras={Palabras}/>
+
 
             
     </>
